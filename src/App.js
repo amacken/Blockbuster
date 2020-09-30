90909a9065562961fd3c8598cf4d9f1e24e23aec
amacken <alex.mackenzie2@gmail.com>
2020-09-30 16:57:43 -0400

    Removed unnecessary info from initial download

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Blockbuster</h1>
     </div>
   );
 }
