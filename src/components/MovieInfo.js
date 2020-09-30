564f47b4f8642419f9cd1a2ceb3afd74b05cabd3
amacken <alex.mackenzie2@gmail.com>
2020-09-30 19:21:36 -0400

    Updated Movie Info component

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -3,8 +3,12 @@ import React from 'react';
 export default function MovieInfo(props){
     return (
         <>
-            <div>
+            <div className="movie-info">
                 <h1>{props.movie.Title}</h1>
+                <img src={props.movie.Poster} alt={props.movie.Title} />
+				<h3>Genre: {props.movie.Genre}</h3>
+				<h4>Plot: {props.movie.Plot}</h4>
+				<h4>{props.movie.Runtime}</h4>
             </div>
         </>
     )
