00a1ffb582bce63ae5f14333fdbb4a6db071fa7c
amacken <alex.mackenzie2@gmail.com>
2020-10-01 15:49:12 -0400

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -11,17 +11,32 @@ import {
 // import * as serviceWorker from './serviceWorker';
 
 const routes = [
+  {
+    path: '/',
+    component: App,
+    name: 'Home'
+  }
+];
 
-]
+const anchor = document.getElementById('root');
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Switch>
+      {routes.map(route => {
+        return (
+          <Route
+            component={route.component}
+            key={route.name}
+            path={route.path}
+          />
+        );
+      })}
+    </Switch>
+  </Router>,
+  anchor
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  // document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
