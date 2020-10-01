58233e1f37590af33647e98dc3d18644db33adee
amacken <alex.mackenzie2@gmail.com>
2020-09-30 22:54:05 -0400

    updated server.js

--- server.js
+++ server.js
@@ -21,8 +21,15 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Controller
+app.use('/api', require('./controllers/movies'));
 
 // Listener
+app.use('/:id/', express.static('public'));
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(`${__dirname}/public/index.html`));
+})
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
