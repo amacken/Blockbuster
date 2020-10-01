cdc3d9752d9b1379e93d989f102ee368096afb4b
amacken <alex.mackenzie2@gmail.com>
2020-10-01 11:51:34 -0400

    Movie index route created

--- controllers/movies.js
+++ controllers/movies.js
@@ -4,6 +4,13 @@ const Movie = require('../models/movie');
 
 // Add Routes
 // Index
+router.get('/', (req, res) => {
+    Movie.find({}, (error, allMovies) => {
+        error ?
+        res.status(404).json(error):
+        res.status(200).json(allMovies)
+    });
+});
 
 // Delete
 
