0872700b75bbfe1820af1810163e693fa6e1e18e
amacken <alex.mackenzie2@gmail.com>
2020-09-30 23:36:57 -0400

    Added movie controller boilerplate

---
+++ controllers/movies.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const Movie = require('../models/movie');
+
+// Add Routes
+// Index
+
+// Delete
+
+// Update
+
+// Create
+
+// Show
+
+// export router
+module.exports = router;
