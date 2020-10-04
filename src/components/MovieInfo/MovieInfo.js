1891b52bd5d600df5c098fb8cf71bd5aeba56da3
amacken <alex.mackenzie2@gmail.com>
2020-10-03 21:24:55 -0400

    Style updates to MovieInfo

--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -4,10 +4,10 @@ import './MovieInfo.css';
 export default function MovieInfo(props){
     return (
             <div className="movie-info">
-                <h1>{props.movie.Title}</h1>
-                <img src={props.movie.Poster} alt={props.movie.Title} />
-				<h3>Genre: {props.movie.Genre}</h3>
-				<h4>Plot: {props.movie.Plot}</h4>
+                <h3>{props.movie.Title}</h3><br/>
+                <img src={props.movie.Poster} alt={props.movie.Title} /><br/>
+				<h3>Genre: {props.movie.Genre}</h3><br/>
+				<h4>Plot: {props.movie.Plot}</h4><br/>
 				<h4>{props.movie.Runtime}</h4>
             </div>
     )
